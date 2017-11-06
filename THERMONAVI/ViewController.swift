47ff9a7355999ff1810c19753c30a9defa6de35d
土居将史 <b1015210@fun.ac.jp>
2017-11-07 01:28:26 +0900

    Initial Commit

---
+++ THERMONAVI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  THERMONAVI
+//
+//  Created by 土居将史 on 2017/11/07.
+//  Copyright © 2017年 土居将史. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
