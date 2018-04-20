fdae786da8e25248d591f8eb6119d37c74134b73
dossey615 <dossey615@gmail.com>
2018-04-20 19:42:36 +0900

    不要ファイルの削除

--- THERMONAVI/File.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  THERMONAVI
-//
-//  Created by 土居将史 on 2017/11/22.
-//  Copyright © 2017年 土居将史. All rights reserved.
-//
-
-import Foundation
--- THERMONAVI/TitleBerFile.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TitleBerFile.swift
-//  THERMONAVI
-//
-//  Created by 土居将史 on 2017/12/06.
-//  Copyright © 2017年 土居将史. All rights reserved.
-//
-
-import Foundation
